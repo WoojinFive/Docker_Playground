c0b2e75346aecc358c14d5a676f2b13d607ec73a
woojinfive <kh3996@gmail.com>
2025-02-06 22:41:12 -0400

    project done

--- docker_express_practice/index.js
+++ docker_express_practice/index.js
@@ -2,3 +2,8 @@ const express = require('express');
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send('Coding in Docker!');
+});
+
+app.listen(8080, () => console.log('Server is running'));
